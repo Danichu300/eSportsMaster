6574757b7f4a0657def4e3d75b1238dce3b660dd
Daniel Rodriguez Cardell <d.rodriguez@sm2baleares.es>
2018-05-17 12:43:13 +0200

    Include de cabezales

--- noticias/rocket.php
+++ noticias/rocket.php
@@ -7,18 +7,9 @@ and open the template in the editor.
 <html>
     <head>
         <title>Rocket League</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="../css/newsstyle.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <?php include("../archivos_php/cabezales.php"); ?>
         <link rel="shortcut icon" href="img/csgoicon.ico" />
         <link rel="stylesheet" href="../css/rocketcss.css"/>
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="../archivos_js/csgo.js"></script>
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
